a702e47b314d0f23dcbd43f5cd0814d4414c1625
Vince Luk <vince.luk@gmail.com>
2019-05-24 00:39:34 -0700

    updated notebook and small refactory for run_classifier and added file column

---
+++ tests/test_run_classifiers.py
@@ -0,0 +1,30 @@
+import pytest
+import pandas as pd
+import run_classifiers as rc
+import logging
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+class TestRunClassifiers(object):
+
+    @pytest.fixture("class")
+    def in_df(self):
+        d = {"a": [1, 1, 1], "b": [2, 2, 2]}
+        return pd.DataFrame(d)
+
+
+
+
+    def test_create_training_data(self, in_df):
+        x_train, x_test, y_train, y_test = rc.create_training_data(in_df, "b")
+
+        print(x_train)
+        print(y_train)
+        assert len(x_train.columns) == 1, "x has too many columns"
+        assert x_train.columns[0] == "a", "x should only have column a"
+        assert y_train.iloc[0] == 2, "y should have 2's"
+
+
+
+
