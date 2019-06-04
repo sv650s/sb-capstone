434640e0a7bb7a9042498213aa52ea30e2e44c30
Vince Luk <vince.luk@gmail.com>
2019-06-03 20:31:18 -0700

    added timer for vectorize time. Moved Timer into it's own class

---
+++ util/Timer.py
@@ -0,0 +1,57 @@
+import logging
+from datetime import datetime
+# TODO: Refactor this later - I don't like this dependency
+from models.ClassifierRunner import Keys
+
+log = logging.getLogger(__name__)
+
+
+class Timer(object):
+
+    def __init__(self):
+        self.timer_dict = {}
+        self.temp_dict = {}
+
+    def start_timer(self, key: str):
+        log.info(f'Start timer for: {key}')
+        self.temp_dict[key] = datetime.now()
+
+    def end_timer(self, key: str) -> float:
+        """
+        calculates the difference in minutes and return value
+        :param key:
+        :return:
+        """
+        log.info(f'End timer for: {key}')
+        end_time = datetime.now()
+        if key in self.temp_dict.keys():
+            start_time = self.temp_dict[key]
+            diff_mins = round((end_time - start_time).total_seconds() / 50, 2)
+            self.timer_dict[key] = diff_mins
+            log.info(f'Total time for {key}: {self.timer_dict[key]}')
+            self.timer_dict[Keys.TOTAL_TIME_MIN] = self.get_total_time()
+        else:
+            log.info(f'No timer for: {key}')
+            diff_mins = 0
+
+        return diff_mins
+
+    def get_time(self, key: str) -> float:
+        """
+        get time in minutes for particular event
+        :param key:
+        :return:
+        """
+        if key in self.timer_dict.keys:
+            return self.timer_dict[key]
+        return 0.
+
+    def get_total_time(self) -> float:
+        total = 0.0
+        for key, val in self.timer_dict.items():
+            total += val
+        return total
+
+    def merge_timers(self, other):
+        if other:
+            self.timer_dict.update(other.timer_dict)
