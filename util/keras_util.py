25a51e8a8fb5e1dd40c3dfb80bfbdf764657d4b3
vince Luk <vince.luk@gmail.com>
2019-11-09 16:34:43 -0800

    check in keras_util works for sampling but not yet for no sampling

--- util/keras_util.py
+++ util/keras_util.py
@@ -1,4 +1,3 @@
-from sklearn.metrics import roc_curve, auc
 from sklearn.metrics import confusion_matrix, classification_report
 import numpy as np
 import pandas as pd
@@ -15,75 +14,13 @@ import logging
 import os
 from datetime import datetime
 import pickle
-import sys
 
+from util.metric_util import calculate_roc_auc
 
 DATE_FORMAT = '%Y-%m'
 TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 log = logging.getLogger()
-
-
-
-def calculate_roc_auc(y_test: np.ndarray, y_predict: pd.DataFrame):
-    """
-    Caculates the false positive rate, true positive rate, and roc_auc
-    Return:
-      roc_auc - dictinary of AUC values there will be 1 auc per class then a macro
-          and micro for the overall model
-          keys: auc_0 to auc_{n_classes} + micro and macro
-      fpr - dictionary of FPR, each value will be a n_classes x ? interpolated array
-          so you can plt ROC curve for each class
-          keys: 0 to {n_classes} + micro and macro
-      tpr - dictionary of TPR, each value will be a n_classes x ? interpolated array
-          so used to plt ROC curve for each class
-          keys: 0 to {n_classes} + micro and macro
-
-    :param y_test: test labels. should be np array or dataframe
-    :param y_predict: test predictions - this should either be an np.ndarray or DataFrame
-    :return:
-    """
-    if isinstance(y_predict, np.ndarray):
-        y_predict =  pd.DataFrame(y_predict)
-
-
-    n_classes = y_test.shape[1]
-
-    # to compute the micro RUC/AUC, we need binariized labels (y_test) and probability of predictions y_predict_df
-
-    fpr = {}
-    tpr = {}
-    roc_auc = {}
-    for i in np.arange(0, 5):
-        fpr_ndarray, tpr_ndarray, _ = roc_curve(y_test[:, i], y_predict[i].to_list())
-        fpr[str(i)] = fpr_ndarray.tolist()
-        tpr[str(i)] = tpr_ndarray.tolist()
-        roc_auc[f'auc_{i + 1}'] = auc(fpr[str(i)], tpr[str(i)]).tolist()
-
-    # Compute micro-average ROC curve and ROC area
-    fpr_ndarray, tpr_ndarray, _ = roc_curve(y_test.ravel(), y_predict.values.ravel())
-    fpr["micro"] = fpr_ndarray.tolist()
-    tpr["micro"] = tpr_ndarray.tolist()
-    roc_auc["auc_micro"] = auc(fpr["micro"], tpr["micro"]).tolist()
-
-    # Compute macro-average ROC curve and ROC area
-
-    # First aggregate all false positive rates
-    all_fpr = np.unique(np.concatenate([fpr[str(i)] for i in range(n_classes)]))
-
-    # Then interpolate all ROC curves at this points
-    mean_tpr = np.zeros_like(all_fpr)
-    for i in range(n_classes):
-        mean_tpr += np.interp(all_fpr, fpr[str(i)], tpr[str(i)])
-
-    # Finally aveage it and compute AUC
-    mean_tpr /= n_classes
-
-    fpr["macro"] = all_fpr.tolist()
-    tpr["macro"] = mean_tpr.tolist()
-    roc_auc["auc_macro"] = auc(fpr["macro"], tpr["macro"]).tolist()
-
-    return roc_auc, fpr, tpr
 
 
 def unencode(input):
@@ -105,7 +42,14 @@ def unencode(input):
     return ret
 
 
-def preprocess_file(data_df, feature_column, label_column, max_sequence_length = 100, use_oov_token=True):
+def preprocess_file(data_df,
+                    feature_column,
+                    label_column,
+                    report_dir,
+                    max_sequence_length = 100,
+                    use_oov_token=True,
+                    sampler = None
+                    ):
     """
     Preprocessing data file and create the right inputs for Keras models
         Generally using this for more advanced models like GRU, LSTM, CNN as we are embedding as part of this
@@ -123,13 +67,11 @@ def preprocess_file(data_df, feature_column, label_column, max_sequence_length =
     :param label_column: string name of lable column
     :param max_sequence_length: maximum number of words to keep in the review
     :param use_oov_token: Default True. Use a out of vocabulary token for tokenizer
+    :param sampler: imblearn sampler must have fit_resample function
     :return: X_train, X_test, y_train, y_test, tokenizer
     """
     labels = data_df[label_column]
     features = data_df[feature_column]
-
-    print("One hot enocde label data...")
-    y = OneHotEncoder().fit_transform(labels.values.reshape(len(labels), 1)).toarray()
 
     # Pre-process our features (review body)
     if use_oov_token:
@@ -150,9 +92,26 @@ def preprocess_file(data_df, feature_column, label_column, max_sequence_length =
 
     # split our data into train and test sets
     print("Splitting data into training and test sets...")
-    X_train, X_test, y_train, y_test = train_test_split(features_padded, y, random_state=1)
-    print(f'Training X shape {X_train.shape} y shape {y_train.shape}')
-    print(f'Test X shape {X_test.shape} y shape {y_test.shape}')
+    X_train, X_test, y_train_unencoded, y_test_unencoded = train_test_split(features_padded, labels, random_state=1)
+    print(f'Training X shape {X_train.shape} y shape {y_train_unencoded.shape}')
+    print(f'Test X shape {X_test.shape} y shape {y_test_unencoded.shape}')
+
+    if sampler is not None:
+        print(f'Start sampling with {sampler.__class__.__name__}')
+        X_train, y_train_unencoded = sampler.fit_resample(X_train, y_train_unencoded)
+        print(f'Resampled X shape {X_train.shape} y shape {y_train_unencoded.shape}')
+
+        dist = pd.DataFrame(y_train_unencoded).reset_index().groupby(0).count()
+        dist.to_csv(f'{report_dir}/{sampler.__class__.__name__}-{len(labels)}-histogram.csv')
+
+        print(f'Resampled distribution:\n{dist}')
+
+    print(f'Shape of y_train {y_train_unencoded.shape}')
+    print("One hot enocde label data...")
+    # have to convert back to array so it's not a sparse matrix
+    y_train = OneHotEncoder(categories='auto').fit_transform(np.reshape(y_train_unencoded, (len(y_train_unencoded), 1))).toarray()
+    y_test = OneHotEncoder(categories='auto').fit_transform(y_test_unencoded.values.reshape(-1, 1)).toarray()
+
 
     return X_train, X_test, y_train, y_test, t
 
