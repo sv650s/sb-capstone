b3a45ea867810490d8389edef1f7890e8844b768
sv650s <vince.luk@gmail.com>
2020-01-29 16:18:17 -0800

    added comparison notebook between LR and LSTM

--- scripts/syncGoogleDrive.sh
+++ scripts/syncGoogleDrive.sh
@@ -5,17 +5,34 @@
 # a bit cumbersome. Created this script so I use Google Drive Sync to do this for me instead
 #
 
+usage() {
+    echo "`echo $0`: [-n debug]"
+}
+
+while getopts "d" opt; do
+    case $opt in
+      d ) echo "Entering DEBUG mode" && DEBUG_FLAG="-n";;
+      \?) echo "Invalid option: -"$OPTARG"" >&2
+          usage
+          exit 1;;
+      : ) echo "Option -"$OPTARG" requires an argument." >&2
+          usage
+          exit 1;;
+    esac
+done
+
 # sync all util file to google colab
 echo "syncing util to google drive..."
-rsync -rauv --delete ../util/*.py ~/Google\ Drive/Springboard/capstone/util/
+rsync -rauv ${DEBUG_FLAG} --delete ../util/*.py ~/Google\ Drive/Springboard/capstone/util/
 
 # sync report files and notebooks from colab to git repot
 echo "pulling down reports..."
-rsync -rauv ~/Google\ Drive/Springboard/capstone/reports/*.csv ../reports/
+rsync -rauv ${DEBUG_FLAG} ~/Google\ Drive/Springboard/capstone/reports/*.csv ../reports/
 
 # actually this is not a good idea - google colab notebooks are slightly different format
 #echo "syncing colab notebooks to git..."
 #rsync -rauv --exclude 5.0-deep-learning-summary.ipynb ~/Google\ Drive/Colab\ Notebooks/amazon-review/*.ipynb ../notebooks/deep_learning
 #echo "syncing deep learning summary notebook to git..."
 #rsync -rauv -n ~/Google\ Drive/Colab\ Notebooks/amazon-review/5.0-deep-learning-summary.ipynb ../notebooks/
+
 
